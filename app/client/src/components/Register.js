57a96dc2a6b307b9ce160386c5e861465c1684d8
andrew <andrew.liang@stonybrook.edu>
2020-06-26 23:30:25 -0400

    register form stable

--- app/client/src/components/Register.js
+++ app/client/src/components/Register.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react'
+import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input'
 import {register} from './ApiFunctions'
 
 
@@ -20,6 +22,7 @@ class Register extends Component{
     handleChange = (e) =>{
         this.setState({[e.target.name]:e.target.value})
         console.log(e.target.value)
+        console.log(this.state)
     }
 
     handleSubmit = (e) =>{
@@ -37,36 +40,38 @@ class Register extends Component{
 
                             <div className="form-group">
                                 <label htmlFor="name">Name</label>
-                                <input type="text" className="form-control" name="name" placeholder="Enter your name"
+                                <input type="text" className="form-control" name="name" placeholder="Enter name"
                                 value={this.state.name} onChange={this.handleChange} />
                             </div>
 
                             <div className="form-group">
                                 <label htmlFor="phone-number">Phone Number</label>
-                                <input type="text" className="form-control" name="phone_number" placeholder="Enter your phone number"
-                                value={this.state.phone_number} onChange={this.handleChange} />
+                                {/* <input type="text" className="form-control" name="phone_number" placeholder="Enter your phone number"
+                                value={this.state.phone_number} onChange={this.handleChange} /> */}
+                                <PhoneInput placeholder="Enter phone number" name="phone_number" defaultCountry="US"
+                                value={this.state.phone_number} onChange={phone_number => this.setState({ phone_number })}/>
                             </div>
 
                             <div className="form-group">
                                 <label htmlFor="dob">Date of Birth</label>
-                                <input type="date" className="form-control" name="dob" placeholder="Enter your date of birth"
+                                <input type="date" className="form-control" name="dob" placeholder="Enter date of birth"
                                 value={this.state.dob} onChange={this.handleChange} />
                             </div>
 
                             <div className="form-group">
                                 <label htmlFor="email">Email</label>
-                                <input type="text" className="form-control" name="email" placeholder="Enter your email"
+                                <input type="text" className="form-control" name="email" placeholder="Enter email"
                                 value={this.state.email} onChange={this.handleChange} />
                             </div>
 
                             <div className="form-group">
                                 <label htmlFor="password">Password</label>
-                                <input type="password" className="form-control" name="password" placeholder="Enter a password"
+                                <input type="password" className="form-control" name="password" placeholder="Enter password"
                                 value={this.state.password} onChange={this.handleChange} />
                             </div>
 
                             <button type="submit" className="btn btn-lg btn-primary btn-block">
-                                Register!
+                                Register
                             </button>
 
                         </form>
