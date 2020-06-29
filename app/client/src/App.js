b06170cc81ac7121016a1d0030b62f62ac19d6fc
andrew <andrew.liang@stonybrook.edu>
2020-06-29 17:48:59 -0400

    add /login route to router

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Register from './components/Register'
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Route exact path="/" component={Register}/>
+        <Route exact path="/login" component={Login} />
       </Router>
     </div>
   );
