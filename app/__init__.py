d478f4c78a9d0e332cf9360156a52799ece0fba6
andrew <andrew.liang@stonybrook.edu>
2020-07-17 19:48:32 -0400

    configuring from instance folder

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,8 @@ from peewee import *
 from flask_cors import CORS
 
 def create_app(__name__):
-    app = Flask(__name__)
-    app.config.from_pyfile('config.py')
+    app = Flask(__name__,instance_relative_config=True)
+    app.config.from_pyfile('config.py',silent=True)
     
     from app.routes import bp as api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
