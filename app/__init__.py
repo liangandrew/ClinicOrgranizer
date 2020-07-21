493ca47500aecf314a774695bd1918a4b6b0ac71
andrew <andrew.liang@stonybrook.edu>
2020-07-21 02:10:55 -0400

    adding cookie configs

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,17 @@ from flask_cors import CORS
 def create_app(__name__):
     app = Flask(__name__,instance_relative_config=True)
     app.config.from_pyfile('config.py',silent=True)
+    app.config.update(
+        SESSION_COOKIE_SECURE=True,
+        SESSION_COOKIE_HTTPONLY=True,
+        SESSION_COOKIE_SAMESITE='Lax',
+    )
     
     from app.routes import bp as api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
 
     CORS(app)
+    #CORS(app,supports_credentials=True)    #need csrf protection for this option?
 
     return app
 
