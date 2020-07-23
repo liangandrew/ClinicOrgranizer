368b9f677e76fe458e2ee4a4e3cbd9206ba5c946
andrew <andrew.liang@stonybrook.edu>
2020-07-23 02:17:15 -0400

    added appointment screen component for edit

---
+++ app/client/src/components/AppointmentScreen.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {Redirect} from 'react-router-dom';
+
+class AppointmentScreen extends Component{
+    state={
+        id:'',
+        org:'',
+        patient:'',
+        patient_email:'',
+        date_created:'',
+        start_time:'',
+        reason:''
+    }
+
+
+}
