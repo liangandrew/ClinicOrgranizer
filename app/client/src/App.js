335b532833e1dada5a27bbb3b5bd8a0a850b4430
andrew <andrew.liang@stonybrook.edu>
2020-07-26 02:51:55 -0400

    getting list of appointments moved to app.js

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Welcome from "./components/Welcome";
 import Navbar from "./components/Navbar";
 import Profile from "./components/Profile";
 import AppointmentScreen from "./components/AppointmentScreen";
+import {getAppointments} from "./components/ApiFunctions";
+
 import axios from 'axios';
 
 
@@ -14,7 +16,9 @@ class App extends Component {
   constructor(){
     super()
     this.state={
-      logged_in:false
+      logged_in:false,
+      fetched_data:false,
+      appointments:[]
     }
   }
 
@@ -38,16 +42,34 @@ class App extends Component {
 
   componentDidMount(){
     this.checkAuth()
+    // if(this.state.logged_in){
+    //   getAppointments().then(data=>{
+    //     this.setState({appointments:data.appointments})
+    //     console.log(data.appointments)
+    //   })
+    // }
+  }
+
+  componentDidUpdate(){
+    if(this.state.logged_in && !this.state.fetched_data){
+      
+      getAppointments().then(data=>{
+        if(data.success){
+          this.setState({appointments:data.appointments, fetched_data:true})
+          console.log(data)
+        }
+        
+      })
+    }
   }
 
   navbarLogout=()=>{
-    axios.get('api/logout',{withCredentials:true}).then(res=>{
+    axios.get('/api/logout',{withCredentials:true}).then(res=>{
       console.log(res)
       if(res.data.success){
         this.setState({
           logged_in:false
         })
-        // return <Redirect to="/"/>
       }
       console.log("state after logging out")
       console.log(this.state.logged_in)
@@ -69,7 +91,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/register" render={(props) => <Register {...props} isAuthenticated={this.state.logged_in}/>} />
             <Route exact path="/login" render={(props) => <Login {...props} isAuthenticated={this.state.logged_in} appLogin={this.handleLogin}/>}  />
-            <Route exact path="/profile" render={(props) => <Profile {...props} isAuthenticated={this.state.logged_in}/>} />
+            <Route exact path="/profile" render={(props) => <Profile {...props} isAuthenticated={this.state.logged_in} appointments={this.state.appointments}/>} />
             <Route path="/appointments/:id" render={(props) => <AppointmentScreen {...props} isAuthenticated={this.state.logged_in}/>} />
             <Route path="/:any" component={Welcome}/>
           </Switch>
--- app/client/src/components/Profile.js
+++ app/client/src/components/Profile.js
@@ -1,28 +1,29 @@
 import React, {Component} from 'react';
 import { Redirect, Link } from "react-router-dom";
-import {getAppointments} from "./ApiFunctions";
+// import {getAppointments} from "./ApiFunctions";
 
 class Profile extends Component{
-    _isMounted=false;
-    state={
-        appointments:[]
-    }
+    // _isMounted=false;
+    // state={
+    //     appointments:[]
+    // }
 
-    componentDidMount(){
-        this._isMounted = true;
-        let apt=getAppointments().then(data=>{
-            if(this._isMounted){
-                this.setState({appointments:data.appointments})
-                console.log(data.appointments)
-            }
+    // componentDidMount(){
+    //     this._isMounted = true;
+    //     getAppointments().then(data=>{
+    //         if(this._isMounted){
+    //             this.setState({appointments:data.appointments})
+    //             console.log(data.appointments)
+    //         }
             
-        })
+    //     })
         
-    }
+    // }
 
-    componentWillUnmount() {
-        this._isMounted = false;
-      }
+    // componentWillUnmount() {
+    //     this._isMounted = false;
+    //   }
+
 
     render(){
         if(!this.props.isAuthenticated){
@@ -32,11 +33,34 @@ class Profile extends Component{
             <div className="container">
                 <h1>Profile</h1>
                 <br/>
-                {this.state.appointments && this.state.appointments.map(apt=>(
-                    <Link to={'/appointments/'+apt.ap_id} key={apt.ap_id}>
-                        {apt.ap_id}
-                    </Link>
-                ))}
+                <div className="card">
+                    <div className="card-header">
+                        <div className="row">
+                            <span className="card-title col s3">ID #</span>
+                            <span className="card-title col s3">Date Created</span>
+                            <span className="card-title col s3">Organization</span>
+                            <span className="card-title col s3">Start Time</span>
+                        </div>
+                    </div>
+                    <div className="card-body">
+                        {this.props.appointments && this.props.appointments.map(apt=>{
+                            let created_date=new Date(apt.created)
+                            // console.log(created_date.toString())
+                            let start_time=new Date(apt.start_time)
+                            // console.log(start_time.toLocaleString())
+                            return(
+                            <Link key={apt.ap_id} to={'/appointments/' + apt.ap_id} style={{ textDecoration: 'none' }}>
+                                <div className="row">
+                                    <span className="card-title col s3">{apt.ap_id}</span>
+                                    <span className="card-title col s3">{created_date.toLocaleString()}</span>
+                                    <span className="card-title col s3">{apt.o.name}</span>
+                                    <span className="card-title col s3">{start_time.toLocaleString()}</span>
+                                </div>
+                            </Link>
+                            )
+                        })}
+                    </div>
+                </div>
             </div>
         )
     }
